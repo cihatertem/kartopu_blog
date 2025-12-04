159a6b4a3c553622ec47cbd6d2d0635761ead4e4
Cihat Ertem <cihatertem@gmail.com>
2025-12-04 09:56:05 +0300

    - Switched Django v6 from 5.2 - django-upgrade added as dev-dep - settings.py modified with django-upgrade.

--- config/settings.py
+++ config/settings.py
@@ -132,5 +132,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
