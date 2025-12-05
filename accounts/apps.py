8c4b4b6fb689f42f0a92f5510d0ce8cc314a13cb
Cihat Ertem <cihatertem@gmail.com>
2025-12-05 15:48:09 +0300

    - Django v6 cleaning:   - apps.py: no more need 'default_auto_field'

--- accounts/apps.py
+++ accounts/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"  # type: ignore
     name = "accounts"
 
     def ready(self):
