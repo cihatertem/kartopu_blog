5b7ab8a46d8e848fafbe81feeba89bdcabe35a77
Cihat Ertem <cihatertem@gmail.com>
2026-01-12 14:35:30 +0300

    - static & media root goes to DEBUG=True only - sitemap cached 1h

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import index, sitemap
 from django.urls import include, path
+from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 
 from accounts import views as account_views
 from core.sitemaps import sitemaps
+
+SITEMAP_CACHE_SECONDS = 60 * 60  # 1 hour
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,23 +35,27 @@ urlpatterns = [
     path("authors/", include("accounts.urls", namespace="accounts")),
     path("comments/", include("comments.urls", namespace="comments")),
     path(
-        "sitemap.xml",
-        index,
-        {"sitemaps": sitemaps},
-        name="django.contrib.sitemaps.views.index",
-    ),
-    path(
-        "sitemap-<section>.xml",
-        sitemap,
-        {"sitemaps": sitemaps},
-        name="django.contrib.sitemaps.views.sitemap",
-    ),
-    path(
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
         name="robots",
     ),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    path(
+        "sitemap.xml",
+        cache_page(SITEMAP_CACHE_SECONDS)(index),
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.index",
+    ),
+    path(
+        "sitemap-<section>.xml",
+        cache_page(SITEMAP_CACHE_SECONDS)(sitemap),
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
