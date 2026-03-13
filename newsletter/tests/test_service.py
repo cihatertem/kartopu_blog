002f7fb473a3dd0572137bf69072426507e5a8c6
Cihat Ertem <cihatertem@gmail.com>
2026-03-13 20:45:40 +0300

    Missing tests for send_subscribe_confirmation

--- newsletter/tests/test_service.py
+++ newsletter/tests/test_service.py
@@ -1,10 +1,44 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
-from newsletter.services import build_subscribe_confirm_url, build_unsubscribe_url
+from newsletter.services import (
+    build_subscribe_confirm_url,
+    build_unsubscribe_url,
+    send_subscribe_confirmation,
+)
 from newsletter.tokens import parse_token
 
 
 class ServicesTest(TestCase):
+    @patch("newsletter.services.send_templated_email")
+    @patch("newsletter.services.build_unsubscribe_url")
+    @patch("newsletter.services.build_subscribe_confirm_url")
+    def test_send_subscribe_confirmation(
+        self, mock_build_confirm, mock_build_unsub, mock_send
+    ):
+        mock_build_confirm.return_value = "http://testserver/confirm"
+        mock_build_unsub.return_value = "http://testserver/unsubscribe"
+
+        email = "test@example.com"
+        send_subscribe_confirmation(email)
+
+        mock_build_confirm.assert_called_once_with(email)
+        mock_build_unsub.assert_called_once_with(email)
+
+        expected_context = {
+            "confirm_url": "http://testserver/confirm",
+            "unsubscribe_url": "http://testserver/unsubscribe",
+            "site_name": "Kartopu Money",
+        }
+
+        mock_send.assert_called_once_with(
+            subject="Newsletter aboneliğinizi onaylayın",
+            to_email=email,
+            template_prefix="subscribe_confirm",
+            context=expected_context,
+        )
+
     def test_build_subscribe_confirm_url(self):
         email = "test@example.com"
         url = build_subscribe_confirm_url(email)
