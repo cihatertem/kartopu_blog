48938de7f0562db0838043caed788cb80b526106
Cihat Ertem <cihatertem@gmail.com>
2026-04-22 00:21:24 +0300

    [[action:merge_dev]] postlist qs simplfied with defer

--- core/services/blog.py
+++ core/services/blog.py
@@ -3,9 +3,19 @@ from blog.models import BlogPost
 
 def published_posts_queryset(*, include_tags: bool = True):
     """Return published posts with common relations attached."""
-    qs = BlogPost.objects.filter(status=BlogPost.Status.PUBLISHED).select_related(
-        "author",
-        "category",
+    qs = (
+        BlogPost.objects.filter(status=BlogPost.Status.PUBLISHED)
+        .select_related(
+            "author",
+            "category",
+        )
+        .defer(
+            "content",
+            "meta_description",
+            "meta_title",
+            "canonical_url",
+            "content_dependencies",
+        )
     )
     if include_tags:
         qs = qs.prefetch_related("tags")
